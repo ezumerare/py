1c318b1b3d5e40dadf116c7b4f9ebb8ab22a4196
nnn <dontlistenthem@protonmail.com>
2025-10-22 17:41:14 +0300

    Update geminiTelegram.py

--- projects/geminiTelegram.py
+++ projects/geminiTelegram.py
@@ -13,38 +13,43 @@ logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 # register the important data
 log = logging.info
-token_bot = ''
-gemini_api_key = ''
+api_key = '8484999536:AAGKfge71XHjlH4Z-ndfggXMEVYmrn9LTtw'
+gemini_api_key = 'AIzaSyAGV1VhyIIf7EPlh4hETwehGELvtOvsmro'
 client = genai.Client(api_key=gemini_api_key)
 
 log(f'Database is connected!')
 log(f'Cursor created!')
 
 dp = Dispatcher()
-bot = Bot(token=token_bot)
+bot = Bot(token=api_key)
 model = "gemini-2.0-flash"
 chat = client.chats.create(model='gemini-2.0-flash')
 
 
-# save all messages to database
-async def save_message(user_id, requests, response):
-        connecting = await aiosqlite.connect('data.db')
-        cursor = await connecting.cursor() 
-        await cursor.execute('INSERT INTO DATA (user, message, response) VALUES (?, ?, ?)', (user_id, requests, response))
-        return await cursor.commit()
-
-# bot listening to all requests
-async def main():
-    await dp.start_polling(bot)
-
-# function
-async def require_message_data(user_id):
-        connecting = await aiosqlite.connect('data.db')
-        cursor = await connecting.cursor() 
+class Data():
+    def __init__(self, connect='', cursor=''):
+        self.connect = connect
+        self.cursor = cursor
         
-        await cursor.execute('SELECT * FROM DATA WHERE user = ?', (user_id,))
-        return await cursor.fetchone()
+    async def db(self):
+        self.connect = await aiosqlite.connect('data.db')
+        self.cursor = await self.connect.cursor()
         
+    async def create_table(self):
+        await self.db()
+        await self.cursor.execute('CREATE TABLE IF NOT EXISTS DATA (user TEXT, requests TEXT, response TEXT)')
+        await self.connect.commit()
+        
+    # save all messages to database
+    async def save_message(self, user_id, requests, response):
+        await self.cursor.execute('INSERT INTO DATA (user, requests, response) VALUES (?, ?, ?)', (user_id, requests, response))
+        return await self.connect.commit()
+
+    # function
+    async def require_message_data(self, user_id):
+        await self.db()
+        await self.cursor.execute('SELECT * FROM DATA WHERE user = ?', (user_id,))
+        return await self.cursor.fetchone()
         
 # started bot command
 @dp.message(Command("start"))
@@ -54,29 +59,29 @@ async def start_handler(message: types.Message):
 # create chat command with history (chats)
 @dp.message(Command("create_chat"))
 async def create_handler(message: types.Message):
-    async with aiosqlite.connect('data.db') as data:
-        cursor = await data.cursor() 
-        
-        user_id = message.from_user.id
+        try:
+            user_id = message.from_user.id
+
+        except aiosqlite.Error as e:
+            log(f'pidor : {e}')
     
-        await cursor.execute('CREATE TABLE IF NOT EXISTS DATA (user TEXT, requests TEXT, response TEXT)')
-    
-        
-        search = require_message_data(user_id)
+        search = await dat.require_message_data(user_id)
         
         try:
             if search is None:
-                await cursor.execute('INSERT OR IGNORE INTO DATA (user, requests, response) VALUES (?, ?, ?)', (user_id, 'requests', 'response'))
+                await dat.cursor.execute('INSERT OR IGNORE INTO DATA (user, requests, response) VALUES (?, ?, ?)', (user_id, 'requests', 'response'))
                 await message.reply("succesfully chat created!")
             
-                save_message(user_id, 'requests', 'response')
+                await dat.save_message(user_id, 'requests', 'response')
                 return
         
             else:
                 await message.reply("you have chat, cannot create new!")
                 
+            await dat.connect.close()
         except aiosqlite.Error as e:
-            log(f'error - {e}')
+            log(f'error  - {e}')
+        
 
         
         
@@ -84,9 +89,10 @@ async def create_handler(message: types.Message):
 @dp.message()
 async def gemini_handler(message: types.Message):
         user_id = message.from_user.id
-    
+        await dat.db()
+        
         try:
-            search = require_message_data(user_id)
+            search = await dat.require_message_data(user_id)
         
             if search is None:
                 await message.reply('you mustn create chat: /create_chat')
@@ -96,12 +102,19 @@ async def gemini_handler(message: types.Message):
             requests = chat.send_message(user_message)
             response = requests.text
             
-            save_message(user_id, requests, response)
+            await dat.save_message(user_id, 'requests', 'response')
             
             await message.reply(response)
         
+            await dat.connect.close()
         except aiosqlite.Error as e:
-            log(f'error - {e}')
-        
+            log(f'error - уидфт {e}')
+
+# bot listening to all requests
+async def main():
+    await dp.start_polling(bot)
+    
+dat = Data()
+
 if __name__ == "__main__":
     asyncio.run(main())
